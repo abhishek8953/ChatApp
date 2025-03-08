67af50ab050f96fe5e3829297b96887ce703a3ba
Abhishek Tiwari <51175997+abhishek8953@users.noreply.github.com>
2025-03-08 13:49:11 +0530

    Update config.js

--- server/constants/config.js
+++ server/constants/config.js
@@ -2,7 +2,7 @@ const corsOptions = {
   origin: [
     "http://localhost:5173",
     "http://localhost:4173",
-    "https://esp-32-realy-control.vercel.app/v2",
+    "https://esp-32-realy-control.vercel.app",
     process.env.CLIENT_URL,
   ],
   methods: ["GET", "POST", "PUT", "DELETE"],
